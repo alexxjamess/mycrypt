1d50ca76d2772e063b24eae82dd793f1d2206cff
alexxjamess <alex.jamess@live.co.uk>
2021-07-28 10:20:30 +0000

    add instructions for how to use tables my predictor and my portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -78,3 +78,38 @@ futureValue1.innerHTML="=No of Coins/Tokens * Future Price";
 percChange1.innerHTML="=(Future Value-Current Value) / Current Value × 100";
 
 }
+/**
+* Function to draw chart of portfolio
+* used from Google Charts 
+*/
+ // Load the Visualization API and the corechart package.
+google.charts.load('current', {'packages':['corechart']});
+          
+// Set a callback to run when the Google Visualization API is loaded.
+google.charts.setOnLoadCallback(drawChart);
+          
+// Callback that creates and populates a data table,
+// instantiates the pie chart, passes in the data and
+// draws it.
+function drawChart() {
+          
+// Create the data table.
+    var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Coin/Token');
+        data.addColumn('number', 'Percentage');
+        data.addRows([
+          ['BTC', 3],
+          ['XRP', 1],
+          ['ADA', 1],
+          ['ETH', 1],
+          ['DOGE', 2]
+]);
+          
+// Set chart options
+    var options = {'title':'My Portfolio',
+                                 'width':400,
+                                 'height':300};
+// Instantiate and draw our chart, passing in some options.
+    var chart = new google.visualization.PieChart(document.getElementById('chart-div'));
+    chart.draw(data, options);
+                }
