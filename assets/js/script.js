92662aece15db54e5908ec6a90325b4672e167f8
alexxjamess <alex.jamess@live.co.uk>
2021-08-04 15:18:07 +0000

    get my portfolio table working correctly apart from precentage of portfolio row

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,17 +16,18 @@ async function getCurrentPriceForCoin(coin, outputCurrency = "usd") {
     const data = await makeAPICall(`${API_BASE_URL}?ids=${coin}&vs_currencies=${outputCurrency}`);
     return data[coin][outputCurrency];
 }
+
 /**Event Listeners for buttons*/
-async function onCurrencySelect() {
-    const selectedCoin = document.getElementById(`coin-token-port`).value;
+async function onCurrencySelect(rowNo) {
+    const selectedCoin = document.getElementById(`coin-token-port-${rowNo}`).value;
     const currentPrice = await getCurrentPriceForCoin(selectedCoin);
-    document.getElementById(`current-price1`).innerHTML = currentPrice;
+    document.getElementById(`current-price-${rowNo}`).innerHTML = currentPrice;
 }
 
-async function onCurrencySelectPred() {
-     const currency = document.getElementById("coin-token-pred").value;
+async function onCurrencySelectPred(rowNo) {
+     const currency = document.getElementById(`coin-token-pred-${rowNo}`).value;
      const currentPrice = await getCurrentPriceForCoin(currency);
-     document.getElementById("current-price").innerHTML = currentPrice;
+     document.getElementById(`current-price-pred-${rowNo}`).innerHTML = currentPrice;
  }
 /**
  * If calculate button is clicked this will run a function
@@ -34,12 +35,12 @@ async function onCurrencySelectPred() {
  * for my portfolio
  */
 function calculateTotalPortfolioValueForEachCoin() {
-    const rows = document.getElementsByClassName("data-row");
-    for (let idx = 0; idx < rows.length; idx++) {
-        const currentPrice = document.getElementById(`current-price1-${idx}`);
-        const coinCount = document.getElementById(`my-amount-input-${idx}`);
-        const totalCoinValue = parseFloat(currentPrice.innerText) * parseFloat(coinCount.value);
-        document.getElementById(`current-value1-${idx}`).innerHTML = totalCoinValue;
+    const rows = document.getElementsByClassName("data-row-port");
+    for (let idx = 1; idx <= rows.length; idx++) {
+        const currentPrice = document.getElementById(`current-price-${idx}`).innerText;
+        const coinCount = document.getElementById(`my-amount-input-${idx}`).value;
+        const totalCoinValue = parseFloat(currentPrice) * parseFloat(coinCount);
+        document.getElementById(`current-value-${idx}`).innerHTML = totalCoinValue;
     }
 }
 /**
@@ -48,90 +49,119 @@ function calculateTotalPortfolioValueForEachCoin() {
  * for my portfolio
  */
  function calculateTotalPredictorValueForEachCoin() {
-  
-      const currentPricePred = document.getElementById(`current-price`);
-      const coinCountPred = document.getElementById(`my-amount-input-pred`);
+    const rows = document.getElementsByClassName("data-row-pred");
+    for (let idx = 1; idx <= rows.length; idx++) {
+
+      const currentPricePred = document.getElementById(`current-price-pred-${idx}`);
+      const coinCountPred = document.getElementById(`my-amount-input-pred-${idx}`);
       const totalCoinValuePred = parseFloat(currentPricePred.innerText) * parseFloat(coinCountPred.value);
-      document.getElementById(`current-value-predictor`).innerHTML = totalCoinValuePred;
+      document.getElementById(`current-value-predictor-${idx}`).innerHTML = totalCoinValuePred;
   }
+
+}
 /**
- * function used to add row to my portfolio table  
+ * function used to add row to my portfolio table
  * inputting the same input fields as prevous row.
  */
-
+//  <tr class="data-row">
+//  <td>
+//    <input type="text" placeholder="Select a Coin/Token" oninput="onCurrencySelect(1)" class="form-input data-row-port" id="coin-token-port-1" name="coin-token" list="coins" />
+//    <datalist id="coins">
+//      <option value="bitcoin">BTC</option>
+//      <option value="ethereum">ETH</option>
+//      <option value="ripple">XRP</option>
+//      <option value="cardano">ADA</option>
+//      <option value="tether">USDT</option>
+//      <option value="dogecoin">DOGE</option>
+//    </datalist>
+//  </td>
+//  <td id="current-price-1" class="data-row-port dollars" ></td>
+//  <td><input required id="my-amount-input-1" placeholder="Enter No of Coins/Tokens" class="form-input data-row-port" type="number" min="0" name="my-amount" /></td>
+//  <td id="current-value-1" class="data-row-port dollars current-total-value" ></td>
+//  <td id="port-value-1" class="data-row-port"></td>
+// </tr>
 function addCoinPortfolio() {
   const currentRowCount = document.getElementById("portfolio-table").rows.length;
   const myPortfolioTable= document.getElementById("portfolio-table");
   // Append a new row to body
-  let newRow = myPortfolioTable.insertRow(2);
+  let newRow = myPortfolioTable.insertRow(currentRowCount);
   newRow.className = "data-row-port";
-      
+
     // Append column 1
     let coinToken = newRow.insertCell(0);
-    coinToken.innerHTML = `<input id="coin-token-port-${currentRowCount + 1}" class="form-input"  placeholder="Select a Coin/Token" type="text" list="coins" oninput="onCurrencySelect(${currentRowCount + 1})"/>`;
+    coinToken.innerHTML = `<input id="coin-token-port-${currentRowCount}" class="form-input"  placeholder="Select a Coin/Token" type="text" list="coins" oninput="onCurrencySelect(${currentRowCount})"/>`;
     // Append column 2
     let coinCurrentPrice = newRow.insertCell(1);
     coinCurrentPrice.className = "dollars current-total-value"
-    coinCurrentPrice.id = `current-price1-${currentRowCount + 1}`
+    coinCurrentPrice.id = `current-price-${currentRowCount}`
     // Append column 3
     let tokenCount = newRow.insertCell(2);
-    tokenCount.innerHTML = `<input id="my-amount-input-${currentRowCount + 1}" class="form-input" placeholder="Enter No of Coins/Tokens" type="number" min="0" />`;
+    tokenCount.innerHTML = `<input id="my-amount-input-${currentRowCount}" class="form-input" placeholder="Enter No of Coins/Tokens" type="number" min="0" />`;
     // Append column 4
     let totalValueCoin = newRow.insertCell(3);
     totalValueCoin.className = "dollars"
+    totalValueCoin.id = `current-value-${currentRowCount}`
        // Append column 5
     let coinPortfolioValue = newRow.insertCell(4);
+    coinPortfolioValue.id = `port-value-${currentRowCount}`
 
 }
 /**
- * function used to add row to my predictor table  
+ * function used to add row to my predictor table
  * inputting the same input fields as prevous row.
  */
 function addCoinPred() {
     const currentRowCountPred = document.getElementById("predictor-table").rows.length;
     const myPredictorTable= document.getElementById("predictor-table");
     // Append a new row to body
-    let newRowPred = myPredictorTable.insertRow(2);
-    newRowPred.className = "data-row-pred-new";
-        
+    let newRowPred = myPredictorTable.insertRow(currentRowCountPred);
+    newRowPred.className = "data-row-pred";
+
       // Append column 1
       let coinTokenPred = newRowPred.insertCell(0);
-      coinTokenPred.innerHTML = `<input id="coin-token-pred-${currentRowCountPred + 1}" class="form-input"  placeholder="Select a Coin/Token" type="text" list="coins" oninput="onCurrencySelectPred(${currentRowCountPred + 1})"/>`;
+      coinTokenPred.innerHTML = `<input id="coin-token-pred-${currentRowCountPred}" class="form-input"  placeholder="Select a Coin/Token" type="text" list="coins" oninput="onCurrencySelectPred(${currentRowCountPred})"/>`;
       // Append column 2
       let coinCurrentPricePred = newRowPred.insertCell(1);
       coinCurrentPricePred.className = "dollars current-total-value";
-      coinCurrentPricePred.id = `current-price-${currentRowCountPred + 1}`;
+      coinCurrentPricePred.id = `current-price-pred-${currentRowCountPred}`;
       // Append column 3
       let tokenCountPred = newRowPred.insertCell(2);
-      tokenCountPred.innerHTML = `<input id="my-amount-input-pred-${currentRowCountPred + 1}" class="form-input" placeholder="Enter No of Coins/Tokens" type="number" min="0" />`;
+      tokenCountPred.innerHTML = `<input id="my-amount-input-pred-${currentRowCountPred }" class="form-input" placeholder="Enter No of Coins/Tokens" type="number" min="0" />`;
       // Append column 4
       let totalValueCoinPred = newRowPred.insertCell(3);
       totalValueCoinPred.className = "dollars";
+      totalValueCoinPred.id = `current-value-predictor-${currentRowCountPred}`
       // Append column 5
       let coinFutureValue = newRowPred.insertCell(4);
       coinFutureValue.className = "dollars";
-      coinFutureValue.innerHTML = `<input id="future-price-input-${currentRowCountPred + 1}" class="form-input" placeholder="Enter future Price" type="number" min="0" />`;
+      coinFutureValue.innerHTML = `<input id="future-price-input-${currentRowCountPred}" class="form-input" placeholder="Enter future Price" type="number" min="0" />`;
       // Append column 6
       let coinFutureValueTotal = newRowPred.insertCell(5);
       coinFutureValueTotal.className = "dollars";
-      coinFutureValueTotal.id =`future-value-${currentRowCountPred + 1}`
+      coinFutureValueTotal.id =`future-value-${currentRowCountPred}`
       // Append column 7
       let coinPercentageChange = newRowPred.insertCell(6);
-      coinPercentageChange.id = `coin-percentage-chain-${currentRowCountPred + 1}`;
+      coinPercentageChange.id = `percentage-change-${currentRowCountPred}`;
 
-  
+
   }
 /**
- * function used to calculate future value by 
+ * function used to calculate future value by
  * multiplying future price inputted by user and no of coins inputted by user.
  */
 
 function calculateFutureValue() {
-    const futurePricePred = document.getElementById("future-price-input").value;
-    const myAmountPred = document.getElementById("my-amount-input-pred").value;
+    const rows = document.getElementsByClassName("data-row-pred");
+    for (let idx = 1; idx <= rows.length; idx++) {
+        
+    const futurePricePred = document.getElementById(`future-price-input-${idx}`).value;
+    const myAmountPred = document.getElementById(`my-amount-input-pred-${idx}`).value;
     const resultFutureValue = futurePricePred * myAmountPred;
-    document.getElementById("future-value").innerHTML = resultFutureValue;
+    document.getElementById(`future-value-${idx}`).innerHTML = resultFutureValue;
+    }
 }
+
+
 /**
  * function used to calculate percentage change
  * from current value to future value my predictor page
@@ -144,7 +174,7 @@ function calculatePercentageChange() {
     console.log(resultPercentageChange);
 }
 /**
- * function used to calculate portfolio percentage 
+ * function used to calculate portfolio percentage
  *  Sum of Current Value Per Coin  /Number of Rows
  */
 function calcualtePortfolioPercentagePerCoin() {
@@ -153,13 +183,14 @@ function calcualtePortfolioPercentagePerCoin() {
 /**
  * Function to draw chart of portfolio
  * used from Google Charts
-*/ 
+
 // Load the Visualization API and the corechart package.
 google.charts.load("current", {
     packages: ["corechart"],
 });
+/**
 // Set a callback to run when the Google Visualization API is loaded.
-google.charts.setOnLoadCallback(drawChart);
+// google.charts.setOnLoadCallback(drawChart);
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
@@ -169,11 +200,11 @@ function drawChart() {
     data.addColumn("string", "Coin/Token");
     data.addColumn("number", "Percentage");
     data.addRows([
-        [document.getElementById(`coin-token-port-${0}`).value, document.getElementById(`current-value1-${1}`)],
-        [document.getElementById(`coin-token-port-${1}`).value, document.getElementById(`current-value1"${2}`)],
-        [document.getElementById(`coin-token-port-${2}`).value, document.getElementById(`current-value1"${3}`)],
-        [document.getElementById(`coin-token-port-${3}`).value, document.getElementById(`current-value1"${4}`)],
-        [document.getElementById(`coin-token-port-${4}`).value, document.getElementById(`current-value1"${5}`)],
+        [document.getElementById(`coin-token-port-${0}`).value, document.getElementById(`current-value-${1}`)],
+        [document.getElementById(`coin-token-port-${1}`).value, document.getElementById(`current-value"${2}`)],
+        [document.getElementById(`coin-token-port-${2}`).value, document.getElementById(`current-value"${3}`)],
+        [document.getElementById(`coin-token-port-${3}`).value, document.getElementById(`current-value"${4}`)],
+        [document.getElementById(`coin-token-port-${4}`).value, document.getElementById(`current-value"${5}`)],
     ]);
     // Set chart options
     let options = {
@@ -187,4 +218,4 @@ function drawChart() {
     let chart = new google.visualization.PieChart(document.getElementById("chart-div"));
     chart.draw(data, options);
 }
-
+*/
